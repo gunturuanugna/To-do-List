48bd1a0cc50ad920349f655735f404f1e874fe44
gunturuanugna <anugnagunturu1@gmail.com>
2021-08-23 13:03:29 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Content from './components/Content';
+import Header from './components/Header';
+import './App.css';
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Content/>
+        </div>
+    )
+}
+
+export default App
